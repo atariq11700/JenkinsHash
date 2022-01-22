98e302bf7cc06f30b021810d8c0f0db5284cbe80
Adam Alder <atariq11700@gmail.com>
2022-01-22 00:22:04 -0700

    refactor

--- java/JenkinsHash.java
+++ java/JenkinsHash.java
@@ -1,46 +1,35 @@
 public class JenkinsHash{
+    private static int hash(String key){
+        int key_length = key.length();
+        int hash = 0;
 
-    private String key = "";
-    private int hash = 0;
-    private int key_length;
-
-
-    /**
-     * 
-     * @param key Key to hash
-     */
-    public JenkinsHash(String key){
-        this.key = key;
-        this.key_length = key.length();
-        hash();
-
-    }
-
-    private void hash(){
         for (int i = 0; i < key_length; i++) { 
             hash += Character.toLowerCase(key.charAt(i));
-            hash += ( hash << 10);
+            hash += (hash << 10);
             hash ^= (hash >>> 6);
         }
+
         hash += (hash << 3);
         hash ^= (hash >>> 11);
         hash += (hash << 15);
+
+        return hash;
     }
 
     /**
      * 
      * @return the hash in a hex string
      */
-    public String get_hash_string(){
-        return Long.toHexString(Integer.toUnsignedLong(hash));
+    public static String get_hash_string(String key){
+
+        return Long.toHexString(Integer.toUnsignedLong(hash(key)));
     }
 
     /**
      * 
      * @return the hash as a unsigned 32bit
      */
-    public Long get_hash_long(){
-        return Integer.toUnsignedLong(hash);
+    public static Long get_hash_long(String key){
+        return Integer.toUnsignedLong(hash(key));
     }
-
 }
